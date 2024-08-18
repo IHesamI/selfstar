c3b244599affe71297449c23793323e7de9051ab
hesam <bhesam2000@yahoo.com>
2024-08-19 00:20:43 +0430

    try to fix build setting

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+// import path, { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "dev-build",
   },
-  root: "./",
   preview: {
     port: 3000,
   },
   plugins: [react()],
   server: {
-    open: true,
     port: 3000,
-    host: "0.0.0.0",
   },
 
   define: {
