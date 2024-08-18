a1ef3fb44dc47d67ded02fc007703f6b6c828964
hesam <bhesam2000@yahoo.com>
2024-08-19 00:32:25 +0430

    change dockerFile and

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "dev-build",
+    outDir: "build",
   },
-  preview: {
-    port: 3000,
-  },
+
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
 
   define: {
     // global: {},
